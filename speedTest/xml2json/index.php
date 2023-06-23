deb01bb7d36e1d5d706da5f3e1358604ae932418
Ahmad Ridho <ahmadridhor0@gmail.com>
2023-06-23 13:59:03 +0800

    modified:   speedTest/transition/index.html 	modified:   speedTest/xml2json/index.php

--- speedTest/xml2json/index.php
+++ speedTest/xml2json/index.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>xml 2 json</title>
 </head>
+
 <body>
 
-<div>
-    <?php 
-        echo simplexml_load_file('data.xml')
-    ?>
-</div>
+    <div>
+        <?php
+        echo simplexml_load_file('data.xml');
+        ?>
+    </div>
 
-<div>
-    <?php 
-        
-    ?>
-</div>
+    <div>
+        <?php
+        echo json_encode(simplexml_load_file('data.xml'));
+        ?>
+    </div>
 
 
 </body>
+
 </html>
